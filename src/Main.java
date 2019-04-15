2a2c96b1ee8539abfede91feb74954bc53ed00b7
wjc <jicheng_wang@sina.com>
2019-04-15 23:46:23 +0800

    iterator

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		BookShelf bookShelf = new BookShelf(4);
 		bookShelf.appenBook(new Book("AAAAA"));
-		bookShelf.appenBook(new Book("BBBB"));
-		bookShelf.appenBook(new Book("CCCC"));
+		bookShelf.appenBook(new Book("BBBBB"));
+		bookShelf.appenBook(new Book("CCCCC"));
 		bookShelf.appenBook(new Book("DDDDD"));
 		Iterator it = bookShelf.iterator();
 		while(it.hasNext()){
